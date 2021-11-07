22c79adcb89dbe448805df86d2564e9b5ad03efd
AnderMoldes <ander.moldes@opendeusto.es>
2021-11-07 13:26:14 +0100

    cambios en los .py

--- ConcesionarioDjango/Project/concesionarioDjango/appConcesionarioDjango/urls.py
+++ ConcesionarioDjango/Project/concesionarioDjango/appConcesionarioDjango/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
-    path('marca/<int:marca_id>/', views.marca, name='detail'),
-    path('departamento/<int:departamento_id>/empleados', views.empleados, name='empleados'),
-    path('empleado/<int:empleado_id>', views.empleado, name='empleado'),
-    path('habilidad/<int:habilidad_id>', views.habilidad, name='habilidad')
+    path('marca/<int:marca_id>/', views.marca, name='marca'),
+    path('marca/<int:marca_id>/coches', views.coches, name='coches'),
+    path('coche/<int:coche_id>', views.coche, name='coche'),
+    path('categoria/<int:categoria_id>', views.categoria, name='categoria')
 ]
--- ConcesionarioDjango/Project/concesionarioDjango/appConcesionarioDjango/views.py
+++ ConcesionarioDjango/Project/concesionarioDjango/appConcesionarioDjango/views.py
@@ -1,34 +1,45 @@
-from django.http import HttpResponse, Http404
+
 from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import render
 from .models import Marca, Categoria, Coche
 
 #devuelve el listado de empresas
 def index(request):
-	marcas = Marca.objects.order_by('nombre')
-	output = ', '.join([m.nombre for m in marcas])
-	return HttpResponse(output)
+	marcas = get_list_or_404(Marca.objects.order_by('nombre'))
+	context = {'lista_marcas': marcas }
+	return render(request, 'index.html', context)
 
 #devuelve los datos de una marca
 def marca(request, marca_id):
-	marca = Marca.objects.get(pk=marca_id)
-	output = ', '.join([str(marca.id), marca.nombre, str(marca.anyoDeCreacion), marca.fundador])
-	return HttpResponse(output)
+	marca = get_object_or_404(Marca, pk=marca_id)
+	#output = ', '.join([str(marca.id), marca.nombre, str(marca.anyoDeCreacion), marca.fundador])
+	context = {'marca': marca }
+	return render(request, 'marca.html', context)
 
 #devuelve los coches de una marca
 def coches(request, marca_id):
-	marca = Marca.objects.get(pk=marca_id)
-	output = ', '.join([c.nombre for c in marca.coche_set.all()])
-	return HttpResponse(output)
+	marca = get_object_or_404(Marca, pk=marca_id)
+	coches =  marca.coche_set.all()
+	context = {'marca': marca, 'coches' : coches }
+	return render(request, 'coches.html', context)
+	
 
 #devuelve los detalles de un coche
 def coche(request, coche_id):
-	coche = Coche.objects.get(pk=coche_id)
-	output = ', '.join([str(coche.id), coche.nombre, str(coche.fecha_creacion), str(coche.caballosPotencia), str(coche.marca), str(coche.categoria)])
-	return HttpResponse(output)
+	coche = get_object_or_404(Coche, pk=coche_id)
+	context = { 'coche': coche }
+    return render(request, 'coche.html', context)
+	
+
+	#output = ', '.join([str(coche.id), coche.nombre, str(coche.fecha_creacion), str(coche.caballosPotencia), str(coche.marca), str(coche.categoria)])
+	#categorias = ', '.join([c.nombre for c in coche.categorias.all()])
+	#output = f"{datos_coche} // Categoria: {categorias}"
+	
 
 #devuelve los detalles de una categoria
 def categoria(request, categoria_id):
-	categoria = Categoria.objects.get(pk=categoria_id)
-	output = ', '.join([str(categoria.id), categoria.nombre, str([c.nombre for c in categoria.coche.all()])])
-	return HttpResponse(output)
+	categoria = get_object_or_404(Categoria, pk=categoria_id)
+	coches =  categoria.coche_set.all()
+	context = { 'coches': coches, 'categoria' : categoria }
+    return render(request, 'categoria.html', context)
 

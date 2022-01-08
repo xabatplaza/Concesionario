e7ed49d2f7b405af25fad9c42a0aef25978c79fe
juanillojr <juan.sanz@opendeusto.es>
2022-01-08 12:33:54 +0100

    formularios de registro y login, falta estilo

--- ConcesionarioDjango/Project/concesionarioDjango/appConcesionarioDjango/urls.py
+++ ConcesionarioDjango/Project/concesionarioDjango/appConcesionarioDjango/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
     path('categoria/<int:categoria_id>', views.categoria, name='categoria'),
 
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='templates/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='templates/logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
